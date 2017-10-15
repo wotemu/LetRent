7f474fe9f1c8e639248a5ba503795acbc195e0b0
Leonid Dashko <all-cs.info@i.ua>
2017-10-15 04:35:55 +0300

    Add and apply "layouts" stucture to current template

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -4,29 +4,46 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { SearchDetailComponent } from './components/search-detail/search-detail.component';
-import { VideoListComponent } from './components/video-list/video-list.component';
-import { VideoDetailComponent } from './components/video-detail/video-detail.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { LoginComponent } from './components/login/login.component';
 import { PropertyListComponent } from './components/property-list/property-list.component';
 import { PropertyDetailComponent } from './components/property-detail/property-detail.component';
+import { BoxedLayoutComponent } from './layouts/boxed/boxed.component';
+import { BoxedWithSidebarLayoutComponent } from './layouts/boxed-with-sidebar/boxed-with-sidebar.component';
 
 const appRoutes: Routes = [
-  {path: '', component: HomeComponent, pathMatch: 'full'},
-  {path: 'login', component: LoginComponent},
-  {path: 'registration', component: RegistrationComponent},
-  {path: 'search', component: SearchDetailComponent},
-  {path: 'videos', component: VideoListComponent},
-  {path: 'properties', component: PropertyListComponent},
-  {path: 'properties/:slug', component: PropertyDetailComponent},
-  {path: 'videos/:slug', component: VideoDetailComponent},
+  // Routes with "boxed" layout
+  {path: '', component: BoxedLayoutComponent, children: [
+    {path: 'search', component: SearchDetailComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'registration', component: RegistrationComponent},
+    {path: '', component: HomeComponent, pathMatch: 'full'},
+  ]},
+
+  // Routes with "boxed + sidebar" layout
+  {path: '', component: BoxedWithSidebarLayoutComponent, children: [
+    {path: 'properties/:slug', component: PropertyDetailComponent},
+    {path: 'properties', component: PropertyListComponent},
+
+    // Infinitely nested routes don't allow us to "[routeLink]" in .html templates because it throws exception.
+    // So, it's better to use explicit nested routes rather than implicit.
+    // https://stackoverflow.com/questions/44789867/uncaught-in-promise-typeerror-cannot-read-property-component-of-null
+    // {path: 'category', component: PropertyListComponent, children: [
+    //    {path: '**', component: PropertyListComponent}
+    //  ]},
+    {path: 'category/:slug1', component: PropertyListComponent},
+    {path: 'category/:slug1/:slug2', component: PropertyListComponent},
+    {path: 'category/:slug1/:slug2/:slug3', component: PropertyListComponent},
+  ]},
+
+  // Routes with no layout
   {path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(
-      appRoutes
+        appRoutes
     )
   ],
   exports: [
---
+++ client/src/app/layouts/boxed-with-sidebar/boxed-with-sidebar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: 'boxed-with-sidebar.component.html'
+})
+export class BoxedWithSidebarLayoutComponent {
+  constructor() {
+  }
+}
